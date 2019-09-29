fb7d9f62c6d4448d69cc6870072860cb5073ecf9
Gaida Escobar <gescobar129@gmail.com>
2019-09-28 23:28:25 -0400

    Done.

---
+++ db/migrate/004_change_datatype_for_opening_closing_times.rb
@@ -0,0 +1,6 @@
+class ChangeDatatypeForOpeningClosingTimes < ActiveRecord::Migration[5.1]
+    def change 
+        change_column :costume_stores, :closing_time, :datetime
+        change_column :costume_stores, :opening_time, :datetime
+    end
+end
